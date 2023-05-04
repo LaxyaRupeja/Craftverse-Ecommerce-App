276a6a13bda81e4bc0ad96ab781f2f6b9346f69b
Laxya Rupeja <altlakshi@gmail.com>
2023-05-04 23:24:17 +0530

    frontend

---
+++ frontend/scripts/index.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
+var sweper = new Swiper(".mySweper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
